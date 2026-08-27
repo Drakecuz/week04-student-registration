d481745cd55ed440968087f7eb3568a01d4af3d1
Drake Natividad <drakedylannatividad17@gmail.com>
2026-08-27 14:38:32 +0800

    feat: create student migration and model

---
+++ database/migrations/2026_08_27_063237_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_id')->unique();
+            $table->string('first_name', 100);
+            $table->string('middle_name', 100)->nullable();
+            $table->string('last_name', 100);
+            $table->string('email')->unique();
+            $table->string('mobile_number', 15);
+            $table->date('date_of_birth');
+            $table->string('gender', 10);
+            $table->string('program', 100);
+            $table->string('year_level', 20);
+            $table->text('address');
+            $table->string('profile_picture', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
